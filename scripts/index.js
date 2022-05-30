7037201064b8f5e3c3508ede8d1665eed835bf90
xhmyjae <tomatis.mt@gmail.com>
2022-05-30 22:54:22 +0200

    feat: coloring cells on click (mousemove not working)

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 class gridGame
 {
 
-    constructor(grid, colors, level, difficulty, timer, name, scoreboard)
+    constructor(grid, cells, colors, level, difficulty, timer, name, scoreboard)
     {
         this.grid = grid;
+        this.cells = cells;
         this.colors = colors;
         this.level = level;
         this.difficulty = difficulty;
@@ -12,48 +13,61 @@ class gridGame
         this.scoreboard = scoreboard;
     }
 
+    randomPath()
+    {
+        // Generate a random path and return it
+        let path = [];
+        let pathLength = Math.floor(this.cells / 3 + this.level + 1);
+
+    }
+
+    colorCell(cell)
+    {
+        if (isClicking) {
+            cell.addEventListener("mousemove", () => {
+                cell.style.backgroundColor = "#FD6C6C";
+                isClicking = true;
+            });
+        } else {
+            cell.addEventListener("mousedown", () => {
+                cell.style.backgroundColor = "#FD6C6C";
+            });
+        }
+    }
+
     createGrid()
     {
+        let gridSize = 0;
         if (this.difficulty === 1) {
-            let gridSize = 5;
-
-            for (let i = 0; i < gridSize; i++) {
-                let row = document.createElement("tr");
-                this.grid.appendChild(row);
-                for (let j = 0; j < gridSize; j++) {
-                    let cell = document.createElement("td");
-                    row.appendChild(cell);
-                }
-            }
+            gridSize = 5;
         } else if (this.difficulty === 2) {
-            let gridSize = 6;
-
-            for (let i = 0; i < gridSize; i++) {
-                let row = document.createElement("tr");
-                this.grid.appendChild(row);
-                for (let j = 0; j < gridSize; j++) {
-                    let cell = document.createElement("td");
-                    row.appendChild(cell);
-                }
-            }
+            gridSize = 6;
         } else {
-            let gridSize = 8;
+            gridSize = 8;
+        }
 
-            for (let i = 0; i < gridSize; i++) {
-                let row = document.createElement("tr");
-                this.grid.appendChild(row);
-                for (let j = 0; j < gridSize; j++) {
-                    let cell = document.createElement("td");
-                    row.appendChild(cell);
-                }
+        for (let i = 0; i < gridSize; i++) {
+            let row = document.createElement("tr");
+            this.grid.appendChild(row);
+            for (let j = 0; j < gridSize; j++) {
+                let cell = document.createElement("td");
+                cell.classList.add("cell");
+                row.appendChild(cell);
             }
         }
 
+        this.cells = document.querySelectorAll(".cell");
     }
 
 }
 
-let grid = document.querySelector("#grid-table")
+let grid = document.querySelector("#grid-table");
 
-let game = new gridGame(grid, 0, 0, 1, 0, 0, 0);
+let game = new gridGame(grid, 0, 0, 0, 1, 0, 0, 0);
 game.createGrid();
+
+let isClicking = false;
+
+game.cells.forEach(cell => {
+    game.colorCell(cell);
+});
